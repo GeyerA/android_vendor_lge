c692ab1c133b48a443042ca503a3e04cb527e384
Andreas Geyer <calo.geyer@gmail.com>
2014-06-16 22:07:31 +0200

    Update device-vendor.mk
    Added embedded SU

--- mako/device-vendor.mk
+++ mako/device-vendor.mk
@@ -17,3 +17,6 @@ LOCAL_STEM := mako/device-partial.mk
 $(call inherit-product-if-exists, vendor/lge/mako/broadcom/$(LOCAL_STEM))
 $(call inherit-product-if-exists, vendor/lge/mako/lge/$(LOCAL_STEM))
 $(call inherit-product-if-exists, vendor/lge/mako/qcom/$(LOCAL_STEM))
+
+
+SUPERUSER_EMBEDDED := true
